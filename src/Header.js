d974332f081978e00930c547b689d8811583054f
skiirux <mohitkumar637@gmail.com>
2021-07-05 12:28:09 +0530

    Created ‘Header’ Component

---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const Header = function () {
+    return (
+        <div className="header">
+            Phone Directory
+        </div>
+    )
+}
+
+//class compomnent
+
+// class Header extends Component {
+//     render() {
+//         return (
+//             <div className="header">
+//                 Phone Directory
+//             </div>
+//         )
+//     }
+//  }
